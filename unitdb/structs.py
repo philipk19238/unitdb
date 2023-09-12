6253150f5a1b9f10bef6c57590904026c2f5b83b
Philip Kung <pkung67@utexas.edu>
2023-09-12 05:04:23 -0700

    fixing formatting

--- unitdb/structs.py
+++ unitdb/structs.py
@@ -5,6 +5,7 @@ class BM25Config(NamedTuple):
     """
     NamedTuple to hold the configuration parameters for the BM25 algorithm.
     """
+
     k1: float  # Controls how quickly an increase in term frequency leads to term saturation.
     b: float  # Determines the scaling by document length: 0.0 means no scaling, 1.0 means fully scaling.
     delta: float  # Controls the scaling of the BM25+ variant.
@@ -14,6 +15,7 @@ class FullTextSearchResult(NamedTuple):
     """
     NamedTuple to hold the result of a full text search.
     """
+
     ref_id: int  # The reference ID of the document.
     bm25: float  # The BM25 score of the document.
 
@@ -22,5 +24,6 @@ class SemanticSearchResult(NamedTuple):
     """
     NamedTuple to hold the result of a semantic search.
     """
+
     ref_id: int  # The reference ID of the document.
     distance: int  # The semantic distance from the query.
